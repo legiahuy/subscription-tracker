5186f7c3af38d6df0855a3ddb48c5f6fab1fb8b5
bonpaul <legiahuy51tambinh@gmail.com>
2025-02-18 17:08:28 +0700

    Add Reminder Workflow

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import authRouter from './routes/auth.routes.js'
 import connectToDatabase from './database/mongodb.js'
 import errorMiddleware from './middlewares/error.middleware.js'
 import cookieParser from 'cookie-parser'
-import arcjetMiddleware from './controllers/arcjet.middleware.js'
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
+import workflowRouter from './routes/workflow.routes.js'
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use(arcjetMiddleware);
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/workflows', workflowRouter)
 
 app.use(errorMiddleware)
 
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,30 +1,44 @@
 import Subscription from '../models/subscription.model.js'
+import { workflowClient } from '../config/upstash.js'
+import { SERVER_URL } from '../config/env.js'
 
 export const createSubscription = async (req, res, next) => {
-    try {
-        const subscription = await Subscription.create({
-            ...req.body,
-            user: req.user._id,
-        })
+  try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
 
-        return res.status(201).json({ success: true, data: subscription});
-    } catch (error) {
-        next(error);
-    }
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    })
+
+    res.status(201).json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
+  }
 }
 
 export const getUserSubscriptions = async (req, res, next) => {
-    try {
-        if(req.user.id !== req.params.id) {
-            const error = new Error('You are not the owner of this account');
-            error.statusCode = 401;
-            throw error;
-        }
-
-        const subsciptions =  await Subscription.find({user: req.params.id});
-
-        return res.status(200).json({success: true, data: subsciptions});
-    } catch (error) {
-        next(error);
+  try {
+    // Check if the user is the same as the one in the token
+    if(req.user.id !== req.params.id) {
+      const error = new Error('You are not the owner of this account');
+      error.status = 401;
+      throw error;
     }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
+  }
 }
